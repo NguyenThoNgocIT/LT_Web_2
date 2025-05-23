46e0244f363d005d9ee0fbdbb1f52c037ea94ec4
NguyenThoNgocIT <nguyenthongoc22072004@gmail.com>
2025-05-23 23:17:27 +0700

    khởi tạo dự án springboot add dependencies

---
+++ LT_Web2/src/main/java/com/example/LT_Web2/LtWeb2Application.java
@@ -0,0 +1,13 @@
+package com.example.LT_Web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtWeb2Application.class, args);
+	}
+
+}
